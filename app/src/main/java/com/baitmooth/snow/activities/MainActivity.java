a871a5f09d8e3eb7a791555911881c33dccb600f
Donnnno <31142286+Donnnno@users.noreply.github.com>
2021-12-19 16:37:39 +0100

    candybar dashboard

---
+++ app/src/main/java/com/baitmooth/snow/activities/MainActivity.java
@@ -0,0 +1,14 @@
+package com.baitmooth.snow.activities;
+
+import androidx.annotation.NonNull;
+
+import candybar.lib.activities.CandyBarMainActivity;
+
+public class MainActivity extends CandyBarMainActivity {
+
+    @NonNull
+    @Override
+    public ActivityConfiguration onInit() {
+        return new ActivityConfiguration();
+    }
+}
